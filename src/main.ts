763c7db08e7ddd83464d84b863a14a9c56348906
KIMMINJI <amber_lucy@naver.com>
2026-03-30 20:58:13 +0900

    chore : 백엔드 초기세팅

---
+++ src/main.ts
@@ -0,0 +1,42 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import session from 'express-session';
+
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  if (!process.env.SESSION_SECRET) {
+    throw new Error('SESSION_SECRET is not defined');
+  }
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? 'none' : 'lax',
+        maxAge: 3600000,
+      },
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(Number(process.env.BACKEND_PORT));
+}
+bootstrap();
