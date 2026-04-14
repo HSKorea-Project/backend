d67bc7238acebd99db3fafb5631f8a05e763a442
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-04-15 02:11:56 +0900

    feat: 성공 응답 포맷 통일

--- src/global/common/response.interceptor.ts
+++ src/global/common/response.interceptor.ts
@@ -5,11 +5,12 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) => ({
         resultType: 'SUCCESS',
