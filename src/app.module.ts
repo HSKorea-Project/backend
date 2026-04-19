ef5e2dc9cd42fb48e764e0b81811d1726299d302
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-04-19 21:43:36 +0900

    chore: S3 모듈 및 snake_case 네이밍 전략 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InquiryModule } from './inquiry/inquiry.module';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+import { S3Module } from './global/s3/s3.module';
 
 @Module({
   imports:[
@@ -17,12 +21,15 @@ import { AppService } from './app.service';
         username: config.get<string>('DB_USERNAME'),
         password: config.get<string>('DB_PASSWORD'),
         database: config.get<string>('DB_NAME'),
+        namingStrategy: new SnakeNamingStrategy(), // DB 컬럼명 자동으로 Snakecase로 변환
 
         autoLoadEntities: true,
         synchronize: true, // 개발용
         logging: true,
       }),
     }),
+    InquiryModule,
+    S3Module,
   ],
   controllers: [AppController],
   providers: [AppService],
