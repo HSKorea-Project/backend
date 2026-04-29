e83c8e91594567bf11a0e9a8433c7307a8000839
yangwooyoung123 <147824713+yangwooyoung123@users.noreply.github.com>
2026-04-29 21:19:06 +0900

    feat: 전화번호 인증 DTO Swagger 예시 추가

--- src/user/dtos/code.dto.ts
+++ src/user/dtos/code.dto.ts
@@ -1,15 +1,27 @@
-import { IsString, Matches } from 'class-validator';
+import { isString, IsString, Matches } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SendCodeDto {
-  @IsString()
+  @ApiProperty({
+    description: "휴대폰 번호",
+    example: '01012341234',
+  })
   @Matches(/^010\d{8}$/)
   phoneNumber: string;
 }
 
 export class VerifyCodeDto {
+  @ApiProperty({
+    description: "휴대폰 번호",
+    example: "01012341234",
+  })
   @IsString()
   phoneNumber: string;
 
+  @ApiProperty({
+    description: "인증번호",
+    example: "1234",
+  })
   @IsString()
   code: string;
 }
